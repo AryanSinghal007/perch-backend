44a26f53a792be54054556a0ca754f42ac7f5b7d
Aryan Singhal <aryansinghal237@gmail.com>
2025-05-03 23:41:21 +0530

    jwt token auth

---
+++ src/main/java/com/config/JwtTokenValidator.java
@@ -0,0 +1,58 @@
+package com.config;
+import java.io.IOException;
+
+import javax.crypto.SecretKey;
+
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Extract the JWT token from the request header
+        String token = request.getHeader(JwtConstant.JWT_HEADER);
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            // Validate the token and set authentication in the context
+
+            try{
+                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+
+                List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                        .commaSeparatedStringToAuthorityList(authorities);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, grantedAuthorities);
+                
+            } catch (Exception e) {
+                // Handle token validation failure
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
